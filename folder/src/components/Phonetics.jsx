f747fc6da69037bce7cce518f6f70b7aed2b55d9
Carla louro <carlalouro@carlas-imac.home>
2026-06-26 17:17:48 +0100

    Add floating rainbow gradient background animation

--- folder/src/components/Phonetics.jsx
+++ folder/src/components/Phonetics.jsx
@@ -7,7 +7,7 @@ export default function Phonetics(props) {
       {props.phonetic.audio && (
         <audio controls>
           <source src={props.phonetic.audio} type="audio/mpeg" />
-          Your browser does not support the audio element.
+         
         </audio>
       )}
     </div>
